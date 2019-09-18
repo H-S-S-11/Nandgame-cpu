6e64cfe83045a359eff69092e4012a5896edfa82
H-S-S-11 <52609833+H-S-S-11@users.noreply.github.com>
2019-09-18 15:14:05 +0100

    PE1 efficient machine code first draft
    First draft of the machine code for the more efficient algorithm for the project euler 1 challenge

--- rtl/program-roms/pe1_efficient_rom.vhd
+++ rtl/program-roms/pe1_efficient_rom.vhd
@@ -1,3 +1,56 @@
+--the ROM and Program Counter
+--put program in here
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+library work;
+use work.all;
+
+entity program_engine is
+port(
+	jump_address: in std_logic_vector(15 downto 0);
+    jump: in std_logic;
+    cl: in std_logic;
+    counter_reset: in std_logic;
+    pc: out std_logic_vector(15 downto 0);
+    instruction: out std_logic_vector(15 downto 0)
+);
+end program_engine;
+
+architecture mem_array of program_engine is
+
+type sixteen_bit_memory_block is array(65535 downto 0) of std_logic_vector(15 downto 0);
+signal rom: sixteen_bit_memory_block;
+signal address: integer range 0 to 65535;
+
+component counter is
+port(
+	x: in std_logic_vector(15 downto 0);
+    st: in std_logic;
+    cl: in std_logic;
+    sync_reset: in std_logic;
+    
+    q: out std_logic_vector(15 downto 0)    
+    );
+end component;
+
+begin
+
+count1: counter
+port map(
+x=>jump_address,
+st=>jump,
+cl=>cl,
+sync_reset=>counter_reset,
+q=>pc
+);
+
+address<=to_integer(unsigned(pc));
+instruction<=rom(address);
+
+--put program here
+
 rom(0)<=x"0000"; --store 0 in RAM(0)
 rom(1)<=x"8a88";
 rom(2)<=x"0001"; --store 0 in RAM(1)
@@ -68,3 +121,12 @@ rom(59)<=x"90a0"; --data+RAM(2) to a
 
 rom(60)<=x"0000"; --end of program
 rom(61)<=x"0000";
+
+
+
+--end of program
+
+end mem_array;
+
+
+
