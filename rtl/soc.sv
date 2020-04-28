93d3d51d73577fb8fdfac1c33e177c4f336891fc
H-S-S-11 <harry@snell.org.uk>
2020-04-28 23:42:12 +0100

    change output so active LEDs light up

--- rtl/soc.sv
+++ rtl/soc.sv
@@ -1,11 +1,11 @@
 // A SystemVerilog wrapper for the Nandgame CPU, making it easier to extend with other modules
 
 module soc(
-    input logic clk, p_sync_reset,
-    output logic [15:0] seven_seg_o
+    input logic clk, p_sync_reset, byte_sel
+    output logic [7:0] leds_o
 );
 
-logic [15:0] pc_i, j_reg_i, d_reg_i;
+logic [15:0] pc_i, j_reg_i, d_reg_i, seven_seg_i;
     
 cpu core1
 (
@@ -13,8 +13,10 @@ cpu core1
     .pc(pc_i),
     .j_reg(j_reg_i),
     .d_reg_o(d_reg_i),
-    .seven_seg_o(seven_seg_o),
-    .pc_reset(sync_reset_p)
+    .seven_seg_o(seven_seg_i),
+    .pc_reset(p_sync_reset)
 );
+
+assign leds_o = byte_select ? ~seven_seg_i[15:8] : ~seven_seg_i[7:0];
 
 endmodule
